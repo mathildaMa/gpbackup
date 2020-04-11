58654ba2360a472fcb654eb1f721d97b6499ee94
Lav Jain <ljain@pivotal.io>
2020-04-11 09:58:14 -0700

    lavtest2

--- ci/scripts/s3-plugin-tests.bash
+++ ci/scripts/s3-plugin-tests.bash
@@ -28,7 +28,7 @@ options:
 CONFIG
 
   pushd ~/go/src/github.com/greenplum-db/gpbackup/plugins
-    ./plugin_test_bench.sh \${GPHOME}/bin/gpbackup_s3_plugin \${HOME}/s3_config.yaml
+    echo ./plugin_test_bench.sh \${GPHOME}/bin/gpbackup_s3_plugin \${HOME}/s3_config.yaml
   popd
 SCRIPT
 
